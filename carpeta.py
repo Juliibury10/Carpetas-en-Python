dd7ce3601d90343a07164739039d99c5274e7349
Juliibury10 <127346670+Juliibury10@users.noreply.github.com>
2023-03-15 16:57:32 -0300

    Add files via upload

---
+++ carpeta.py
@@ -0,0 +1,12 @@
+#Lenguaje de programacion de cada alumno
+#Repaso de POO (Herencia, Clases). Ejemplo de "Carpetas, fotos, archivos"
+#TDD (Test Unitarios)
+#Github/CI
+
+#----------------------------------------------------------------------------------------------------------------------
+
+class Carpeta:
+    subcarpeta = " "
+    fotos = " "
+    archive = " "
+    
